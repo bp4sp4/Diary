29d776fd40eaeec11b3bfee94b61b1fbbd055793
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-12-12 15:19:35 +0900

    update

---
+++ README.md
@@ -0,0 +1,38 @@
+# Diary Mini Project(React)
+
+## 하는 이유 : 리액트 실력향상, 나만의 블로그 or 일기장
+
+### 디자인 추후 계속 업데이트
+
+<h3> 23/12/12(화)</h3>
+
+- 프로젝트 생성
+- 기본 구조 생성
+- 초기 디자인
+  ![React App](https://github.com/bp4sp4/Diary/assets/62207757/61d2fc02-c9fc-4bb3-b5b2-bb1b8689de73)
+
+```
+$ npm create-react-app diary
+```
+
+- 필요없는 파일 삭제
+  - public 폴더 내에 로고파일들
+  - src 폴더안의 불필요한 파일들을 삭제
+  - app.js 파일을 열어 내용을 깨끗이 비워준다.
+- index.js
+
+```
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+```
+
+- 텍스트 에디터 다운
+
+```
+npm install @ckeditor/ckeditor5-react @ckeditor/ckeditor5-build-classic
+
+```
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./App.module.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Diary</h1>
+      <div className={styles.diary_contianer}>
+        <h2>제목</h2>
+        <div>내용</div>
+      </div>
+      <div className={styles.form_wrapper}>
+        <input className={styles.title_input} type="text" placeholder="제목" />
+        <CKEditor
+          editor={ClassicEditor}
+          style={{ height: "300px !important" }}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+      </div>
+      <div className={styles.buttondisplay}>
+        <button className={styles.submit_button}>입력</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
