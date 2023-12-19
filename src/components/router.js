ca1c8f17ecbb8ef08ce796107933a74477eb967f
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-12-19 21:56:43 +0900

    Update router.js

--- src/components/router.js
+++ src/components/router.js
@@ -3,11 +3,11 @@ import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Character from "./charaters/charaters";
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Character />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
