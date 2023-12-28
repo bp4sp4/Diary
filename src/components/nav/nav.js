595631ffef7e5f151d1bb275f624b44904c76632
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-12-29 03:02:34 +0900

    nav update

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,7 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import styles from "./nav.module.css";
+import { Link } from "react-router-dom";
 function ColorSchemesExample() {
   return (
     <>
@@ -8,7 +9,7 @@ function ColorSchemesExample() {
         <Navbar bg="light" data-bs-theme="light">
           <Navbar.Brand href={process.env.PUBLIC_URL}>나의정보</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="LostArk-Open-API/notice/notice">공지</Nav.Link>
+            <Link to="/notice/notice">공지</Link>
             <Nav.Link href="#features">게임콘텐츠</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
