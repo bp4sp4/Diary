07cff1057b0c75228c9755769fb18a9a3f926116
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-12-29 01:50:18 +0900

    deploy

--- src/components/charaters/charaters.js
+++ src/components/charaters/charaters.js
@@ -12,7 +12,7 @@ function Character() {
           "https://bp4sp4.github.io/LostArk-Open-API/Characters.json"
         );
         const data = await response.json();
-        console.log(data); // Check the data in the console
+
         setCharacterData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -23,28 +23,30 @@ function Character() {
   }, []);
 
   return (
-    <div className={styles.main}>
+    <>
       <ColorSchemesExample />
-      <h1 className={styles.header}>나의캐릭터들</h1>
-      <div className={styles.lost_wrap}>
-        <div className={styles.lost_charater}>
-          {characterData.map((character, index) => (
-            <div key={index} className={styles.characterCard}>
-              <img
-                src={character.CharacterImage}
-                alt={`Character : ${index}`}
-                className={styles.characterImage}
-              />
-              <div className={styles.classinfo}>
-                <p>서버 : {character.ServerName}</p>
-                <p>클래스 : {character.CharacterClassName}</p>
-                <p>아이템 : {character.ItemAvgLevel}</p>
+      <div className={styles.main}>
+        <h1 className={styles.header}>나의캐릭터들</h1>
+        <div className={styles.lost_wrap}>
+          <div className={styles.lost_charater}>
+            {characterData.map((character, index) => (
+              <div key={index} className={styles.characterCard}>
+                <img
+                  src={character.CharacterImage}
+                  alt={`Character : ${index}`}
+                  className={styles.characterImage}
+                />
+                <div className={styles.classinfo}>
+                  <p>서버 : {character.ServerName}</p>
+                  <p>클래스 : {character.CharacterClassName}</p>
+                  <p>아이템 : {character.ItemAvgLevel}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
