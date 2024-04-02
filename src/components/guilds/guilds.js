a819d821a22fd6f38f80fa792df00e5b295c3b82
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2024-04-02 16:00:34 +0900

    guild rank update

--- src/components/guilds/guilds.js
+++ src/components/guilds/guilds.js
@@ -1,6 +1,7 @@
 import Guildsdata from "./guilds.json";
 import React, { useEffect, useState } from "react";
 import styles from "./guilds.module.css";
+import ColorSchemesExample from "../nav/nav";
 
 function Guilds() {
   const [guildsData, setGuildsData] = useState([]);
@@ -10,16 +11,19 @@ function Guilds() {
   }, []);
 
   return (
-    <div className={styles.guildsContainer}>
+    <div>
+      <ColorSchemesExample />
       <h1>Guilds Ranking</h1>
-      <ul className={styles.guildsList}>
-        {guildsData.map((guild) => (
-          <li key={guild.Rank}>
-            <span>{guild.Rank}</span>
-            <span>{guild.GuildName}</span>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.main}>
+        <ul className={styles.guildsList}>
+          {guildsData.map((guild) => (
+            <li key={guild.Rank}>
+              <span>{guild.Rank}</span>
+              <span>{guild.GuildName}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/router.js
+++ src/components/router.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Character from "./charaters/charaters";
 import Notice from "./notice/notice";
-
+import Guilds from "./guilds/guilds";
 const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Character />} />
         <Route path="/notice/notice" element={<Notice />} />
+        <Route path="/guilds/guilds" element={<Guilds />} />
       </Routes>
     </BrowserRouter>
   );
